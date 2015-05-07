2ddb115dc1453b3260c49c4bb8a3e3c55f0016d1
Ben Halverson <benhalverson33@gmail.com>
2015-05-06 21:15:47 -0700

    server setup

---
+++ server.js
@@ -0,0 +1,16 @@
+'use strict';
+var express = require('express');
+// var jade = require('jade');
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+app.set('views', __dirname);
+app.set('view engine', 'jade'); //extension of views
+
+app.get('*', function(req, res){
+  res.render('index');
+  console.info('All is well');
+});
+
+app.listen(3000);
+console.log('server on localhost:3000');
