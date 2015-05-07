963ffc2326ecfe2d114dda663006c408dda54cf2
Ben Halverson <benhalverson33@gmail.com>
2015-05-07 00:05:59 -0700

    broken

--- server.js
+++ server.js
@@ -3,13 +3,18 @@ var express = require('express');
 // var jade = require('jade');
 var app = express();
 
-app.use(express.static(__dirname + '/client/'));
-app.set('views', __dirname + 'views');
+app.use(express.static(__dirname + '/public/'));
+app.set('views', __dirname);
 app.set('view engine', 'jade'); //extension of views
 
-app.get('*', function(req, res){
+app.get('*', function(req, res, err){
   res.render('index');
   console.info('All is well');
+  console.error('Error!' + err);
 });
 
-app.listen(process.env.PORT, process.env.IP);
+
+app.listen(process.env.PORT, process.env.IP, function(port){
+  port = 3000;
+  console.log('port ' + port);
+});
