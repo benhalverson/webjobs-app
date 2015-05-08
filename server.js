fb629e017e70c277ef55a359cef7f7f31e1ed70b
Ben Halverson <benhalverson33@gmail.com>
2015-05-07 19:09:49 -0700

    mongoose and seed setup

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 'use strict';
 var express = require('express');
-// var jade = require('jade');
+var mongoose = require('mongoose');
 var app = express();
 
 app.set('views', __dirname);
@@ -8,10 +8,24 @@ app.set('view engine', 'jade');
 
 app.use(express.static(__dirname + '/public/'));
 
-app.get('*', function(req, res, err){
-  res.render('index');
-  console.info('All is well');
-  console.error('Error!' + err);
+app.get('/api/jobs', function(req, res){
+  res.send('test');
 });
 
-app.listen(process.env.PORT, process.env.IP);
+app.get('*', function(req, res){
+  res.render('index');
+
+mongoose.connect('mongodb://localhost/jobfinder');
+
+var connection = mongoose.connection;
+
+connection.once('open', function(){
+  console.info('connected to mongodb successfully!');
+});
+
+  console.info('Web server started');
+
+});
+
+// app.listen(process.env.PORT, process.env.IP || 3000);
+app.listen(3000);
