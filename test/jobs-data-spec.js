3617be728ad4ddbf5615662ac5684873565cb656
Ben Halverson <benhalverson33@gmail.com>
2015-05-18 21:48:08 -0700

    refactor tests

--- test/jobs-data-spec.js
+++ test/jobs-data-spec.js
@@ -2,13 +2,15 @@ var expect = require("chai").expect;
 var mongoose = require("mongoose");
 var jobModel = require("../models/job.models");
 
-mongoose.connect('mongodb://localhost/jobfinder');
+
 describe("get jobs", function(){
 
-  it("should never be empty since jobs are seeded", function(){
-    mongoose.model('Job').find({}).exec(function(error, jobsList){
-      res.send(jobsList);
-      expect(jobsList.length).to.be.at.least(1);
+  it("should never be empty since jobs are seeded", function(done) {
+    mongoose.connect('mongodb://localhost/jobfinder', function() {
+      mongoose.model('Job').find({}).exec(function(error, jobsList){
+        expect(jobsList.length).to.be.at.least(1);
+        done();
+      });
     });
   });
 });
