4aa7fca1a8434b30c158ee983563f0b09ae3f5f9
Ben Halverson <benhalverson33@gmail.com>
2015-05-18 21:17:46 -0700

    linked backend to front end

--- public/app/app.js
+++ public/app/app.js
@@ -1,10 +1,6 @@
 'use strict';
-angular.module('dailyjobs-app', []);
+angular.module('dailyjobs-app', ['ngResource']);
 
-angular.module('dailyjobs-app').controller('testCtrl', function($scope) {
-  $scope.jobs = [{
-    title: 'Web Developer', description: 'You will code'
-    }, {
-    title: 'Sales Guy', description: 'You will sell stuff'
-    }];
+angular.module('dailyjobs-app').controller('testCtrl', function($scope, $resource) {
+  $scope.jobs = $resource('/api/jobs').query();
 });
